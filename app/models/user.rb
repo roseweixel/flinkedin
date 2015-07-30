30af1aa5004b439101d883754768b48c2d5ae284
Rose Weixel <rose.weixel@gmail.com>
2015-07-30 12:35:26 -0400

    done with avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   
   has_secure_password
+
+  has_attached_file :avatar, :styles => { :large => "500x500>", :medium => "300x300>", :thumb => "100x100>" }
+
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
