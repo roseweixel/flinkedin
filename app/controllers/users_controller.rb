43d7d6bd4bffb49f78d295031b9ecd4551c65c74
Rose Weixel <rose.weixel@gmail.com>
2015-07-31 11:10:33 -0400

    don't require login for users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_login, except: [:new]
+  before_action :require_login, except: [:new, :create]
   before_action :require_user_is_current_user, only: [:edit, :update, :destroy]
 
   # GET /users
